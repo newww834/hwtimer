12e206ccfeb98bf5ebc65e37f2b991b015983102
newww834 <newww834@gmail.com>
2013-11-04 17:32:34 -0600

    Initial Commit

---
+++ hwtimer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hwtimer
+//
+//  Created by improv on 11/4/13.
+//  Copyright (c) 2013 improv. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
